a81357a2ac2dd64d5aa8626594b3d69ca576233f
AbdullahAlMohaimen <mohaimenhasan7@gmail.com>
2023-10-27 23:24:54 +0600

    j

--- Authentication/Home/UserListController.cs
+++ Authentication/Home/UserListController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Authentication.Service;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,29 @@ namespace Authentication.Home
 {
 	public partial class UserListController : UserControl
 	{
+		UserService userService = new UserService();
 		public UserListController()
 		{
 			InitializeComponent();
+
+			this.GetAllUsers();
 		}
+
+		#region Get ALL Users
+		public void GetAllUsers()
+		{
+			List<BO.Users> users = new List<BO.Users>();	
+			try
+			{
+				users = userService.GetUsers();
+				allUserGrid.DataSource = users;
+			}
+			catch (Exception ex)
+			{
+				//MessageBox.Show("Please enter your login ID!", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+				MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+		}
+		#endregion
 	}
 }
