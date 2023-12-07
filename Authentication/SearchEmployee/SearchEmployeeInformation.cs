e7a5f02d37332b3f75ace0039a3db3c14e3a34c5
AbdullahAlMohaimen <mohaimenhasan7@gmail.com>
2023-12-08 00:21:06 +0600

    ff

--- Authentication/SearchEmployee/SearchEmployeeInformation.cs
+++ Authentication/SearchEmployee/SearchEmployeeInformation.cs
@@ -159,7 +159,7 @@ namespace Authentication.SearchEmployee
 		#region Grid Column CLick
 		private void allEmployeeGrid_CellContentClick(object sender, DataGridViewCellEventArgs e)
 		{
-			allEmployeeGrid.Rows[e.RowIndex].DefaultCellStyle.BackColor = Color.LightBlue;
+			//allEmployeeGrid.Rows[e.RowIndex].DefaultCellStyle.BackColor = Color.LightBlue;
 
 			employee = employeeService.GetEmployee(allEmployeeGrid.CurrentRow.Cells["Employee No"].Value.ToString());
 			if (employee != null)
