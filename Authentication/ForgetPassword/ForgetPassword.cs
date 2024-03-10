c036fe807957ce55333377f341ddeba39052a1f5
AbdullahAlMohaimen <mohaimenhasan7@gmail.com>
2024-03-11 00:05:35 +0600

    shadow

--- Authentication/ForgetPassword/ForgetPassword.cs
+++ Authentication/ForgetPassword/ForgetPassword.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,8 +14,89 @@ namespace Authentication
 {
 	public partial class ForgetPassword : Form
 	{
-		UserService userService = new UserService();
+		#region Shadow
+		[DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+		private static extern IntPtr CreateRoundRectRgn
+		(
+			int nLeftRect = 0,
+			int nTopRect = 0,
+			int nRightRect = 0,
+			int nBottomRect = 0,
+			int nWidthEllipse = 0,
+			int nHeightEllipse = 0
+		 );
+		[DllImport("dwmapi.dll")]
+		public static extern int DwmExtendFrameIntoClientArea(IntPtr hWnd, ref MARGINS pMarInset);
+		[DllImport("dwmapi.dll")]
+		public static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, ref int attrValue, int attrSize);
+		[DllImport("dwmapi.dll")]
+		public static extern int DwmIsCompositionEnabled(ref int pfEnabled);
+		private bool m_aeroEnabled;
+		private const int CS_DROPSHADOW = 0x00020000;
+		private const int WM_NCPAINT = 0x0085;
+		private const int WM_ACTIVATEAPP = 0x001C;
+		public struct MARGINS
+		{
+			public int leftWidth;
+			public int rightWidth;
+			public int topHeight;
+			public int bottomHeight;
+		}
+		private const int WM_NCHITTEST = 0x84;
+		private const int HTCLIENT = 0x1;
+		private const int HTCAPTION = 0x2;
+		protected override CreateParams CreateParams
+		{
+			get
+			{
+				m_aeroEnabled = CheckAeroEnabled();
+
+				CreateParams cp = base.CreateParams;
+				if (!m_aeroEnabled)
+					cp.ClassStyle |= CS_DROPSHADOW;
+				return cp;
+			}
+		}
+		private bool CheckAeroEnabled()
+		{
+			if (Environment.OSVersion.Version.Major >= 6)
+			{
+				int enabled = 0;
+				DwmIsCompositionEnabled(ref enabled);
+				return (enabled == 1) ? true : false;
+			}
+			return false;
+		}
+		protected override void WndProc(ref Message m)
+		{
+			switch (m.Msg)
+			{
+				case WM_NCPAINT:
+					if (m_aeroEnabled)
+					{
+						var v = 2;
+						DwmSetWindowAttribute(this.Handle, 2, ref v, 10);
+						MARGINS margins = new MARGINS()
+						{
+							bottomHeight = 0,
+							leftWidth = 0,
+							rightWidth = 0,
+							topHeight = 0
+						};
+						DwmExtendFrameIntoClientArea(this.Handle, ref margins);
+					}
+					break;
+				default:
+					break;
+			}
+			base.WndProc(ref m);
+			if (m.Msg == WM_NCHITTEST && (int)m.Result == HTCLIENT)
+				m.Result = (IntPtr)HTCAPTION;
+		}
+		#endregion
+
 		#region property
+		UserService userService = new UserService();
 		private Form callingForm;
 		public string _loginID;
 		public string LoginID
---
+++ Authentication/Shadow.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Authentication
+{
+	public class Shadow : Form
+	{
+		#region Shadow
+		[DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+		private static extern IntPtr CreateRoundRectRgn
+		(
+			int nLeftRect,
+			int nTopRect,
+			int nRightRect,
+			int nBottomRect,
+			int nWidthEllipse,
+			int nHeightEllipse
+		 );
+		[DllImport("dwmapi.dll")]
+		public static extern int DwmExtendFrameIntoClientArea(IntPtr hWnd, ref MARGINS pMarInset);
+		[DllImport("dwmapi.dll")]
+		public static extern int DwmSetWindowAttribute(IntPtr hwnd, int attr, ref int attrValue, int attrSize);
+		[DllImport("dwmapi.dll")]
+		public static extern int DwmIsCompositionEnabled(ref int pfEnabled);
+		private bool m_aeroEnabled;
+		private const int CS_DROPSHADOW = 0x00020000;
+		private const int WM_NCPAINT = 0x0085;
+		private const int WM_ACTIVATEAPP = 0x001C;
+		public struct MARGINS
+		{
+			public int leftWidth;
+			public int rightWidth;
+			public int topHeight;
+			public int bottomHeight;
+		}
+		private const int WM_NCHITTEST = 0x84;
+		private const int HTCLIENT = 0x1;
+		private const int HTCAPTION = 0x2;
+		protected override CreateParams CreateParams
+		{
+			get
+			{
+				m_aeroEnabled = CheckAeroEnabled();
+
+				CreateParams cp = base.CreateParams;
+				if (!m_aeroEnabled)
+					cp.ClassStyle |= CS_DROPSHADOW;
+				return cp;
+			}
+		}
+		private bool CheckAeroEnabled()
+		{
+			if (Environment.OSVersion.Version.Major >= 6)
+			{
+				int enabled = 0;
+				DwmIsCompositionEnabled(ref enabled);
+				return (enabled == 1) ? true : false;
+			}
+			return false;
+		}
+		protected override void WndProc(ref Message m)
+		{
+			switch (m.Msg)
+			{
+				case WM_NCPAINT:
+					if (m_aeroEnabled)
+					{
+						var v = 2;
+						DwmSetWindowAttribute(this.Handle, 2, ref v, 10);
+						MARGINS margins = new MARGINS()
+						{
+							bottomHeight = 0,
+							leftWidth = 0,
+							rightWidth = 0,
+							topHeight = 0
+						};
+						DwmExtendFrameIntoClientArea(this.Handle, ref margins);
+					}
+					break;
+				default:
+					break;
+			}
+			base.WndProc(ref m);
+			if (m.Msg == WM_NCHITTEST && (int)m.Result == HTCLIENT)
+				m.Result = (IntPtr)HTCAPTION;
+		}
+		#endregion
+	}
+}
